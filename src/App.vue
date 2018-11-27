d7a23c0a2249d731b63a5f72a6679daafb4f879a
David Meir-Levy <davidmeirlevy@gmail.com>
2018-11-27 10:43:12 +0200

    remove vue router

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
 	<div id="app">
-		<Navbar/>
-		<router-view/>
+		<Home />
 	</div>
 </template>
 <script lang="ts">
     import {Component, Vue} from "vue-property-decorator";
-    import Navbar from "./components/Navbar.vue";
+    import Home from "./views/Home.vue";
 
     @Component({
         components: {
-            Navbar,
+            Home,
         },
     })
     export default class App extends Vue {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 import store from './store'
 import './styles/main.scss';
 import './plugins/element.js'
@@ -12,7 +11,6 @@ Vue.config.productionTip = false;
 Vue.use(ElementUI);
 
 new Vue({
-    router,
     store,
     render: h => h(App)
 }).$mount('#app')
