7716e986f5b056a683f998d94ec42abd913f8146
alonronin <alon@ronin.co.il>
2018-11-27 15:27:41 +0200

    :sparkle: products

---
+++ src/components/Scroller.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="wrapper">
+        <div class="container"><slot></slot></div>
+    </div>
+</template>
+
+<script lang="ts">
+	import {Component, Vue} from "vue-property-decorator";
+
+	@Component
+	export default class Scroller extends Vue {
+	}
+</script>
+
+<style lang="scss" scoped>
+    .wrapper {
+    }
+
+    .container {
+        white-space: nowrap;
+        overflow-y: scroll;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,50 @@
 <template>
     <Layout>
-        MAIN
+        Main
+
+        <Scroller>
+            <el-card :body-style="{ padding: '0px' }" v-for="(item, index) in 50">
+                <img src="https://picsum.photos/100/100" class="image">
+                <p>Product title</p>
+            </el-card>
+        </Scroller>
     </Layout>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from "vue-property-decorator";
     import Layout from "@/components/Layout.vue";
+    import Scroller from "@/components/Scroller.vue";
 
     @Component({
         components: {
             Layout,
+            Scroller
         },
     })
     export default class Home extends Vue {
     }
 </script>
-<style scoped>
+<style lang="scss" scoped>
+    .image {
+        width: 100%;
+        display: block;
+    }
 
+    .el-card {
+        &:first-of-type {
+            margin-left: 0;
+        }
+
+        &:last-of-type {
+            margin-right: 0;
+        }
+
+        margin: 0 10px;
+        display: inline-block;
+
+        p {
+            text-align: center;
+        }
+    }
 </style>
