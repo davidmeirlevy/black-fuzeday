c2b916918b341aeefee86246d9d6ca29b480ab13
Andrey S <novus42@gmail.com>
2018-11-27 15:10:21 +0200

    resolving conflicts

--- src/views/Components.vue
+++ src/views/Components.vue
@@ -1,14 +1,24 @@
 <template>
-
+    <div>
+        <Header title="Hello I'm A header"></Header>
+        <Conversation :messages = "[
+            {author: 'Bot', message: 'Hello'}, 
+            {author: 'User', message: 'I want to buy boots'}, 
+            {author: 'Bot', message: 'Kill all humans'}, 
+            {author: 'User', message: 'Oh no'}
+        ]"></Conversation>
+    </div>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from "vue-property-decorator";
     import Header from '@/components/Header.vue';
+    import Conversation from '@/components/Conversation.vue';
 
     @Component({
         components: {
-            Header
+            Header,
+            Conversation,
         },
     })
 
