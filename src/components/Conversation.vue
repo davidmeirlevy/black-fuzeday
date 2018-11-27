1dfad66099cea24205df49ea1d7563ade90a35b9
Andrey S <novus42@gmail.com>
2018-11-27 15:10:30 +0200

    adding conversation

---
+++ src/components/Conversation.vue
@@ -0,0 +1,39 @@
+<template>
+    <ul>
+        <li v-for="item in messages">
+            <el-card v-bind:class="userMessage(item.author)" class="box-card">
+            <div slot="header" class="clearfix">
+                <span>{{ item.author}}</span>
+            </div>
+            {{ item.message }}
+            </el-card>
+            </li>
+    </ul>
+</template>
+
+<script lang="ts">
+	import {Component, Prop, Vue} from "vue-property-decorator";
+
+	@Component
+	export default class Conversation extends Vue {
+        @Prop() private messages!: Array<any>;
+        
+        userMessage(sender: string) {
+            return sender === 'User' && 'box-right';
+        }
+	}
+</script>
+
+<style scoped>
+    li {
+        list-style-type: none;
+        margin-bottom: 10px;
+    }
+    .box-card {
+        border-radius: 20px;
+    }
+
+    .box-right {
+        text-align: right;
+    }
+</style>
